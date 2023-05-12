6e6e430122dcd436cda81d966756cb076b72fbe4
seawesel <jrodperalta@gmail.com>
2023-05-12 16:00:43 -0400

    experimenting witih graphs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,38 +6,80 @@ import Seo from "../components/seo"
 import { receipts } from '../response.js';
 
 
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 const data = [
-  { name: 'Page A', uv: 400, pv: 400, amt: 2400 },
-  { name: 'Page B', uv: 500, pv: 400, amt: 2500 },
-  { name: 'Page C', uv: 600, pv: 400, amt: 2400 },
-  { name: 'Page D', uv: 700, pv: 400, amt: 2300 },
-  { name: 'Page E', uv: 800, pv: 400, amt: 2400 },
-  { name: 'Page F', uv: 900, pv: 400, amt: 2500 }
+  {
+    subject: "Math",
+    A: 120,
+    B: 110,
+    fullMark: 150
+  },
+  {
+    subject: "Chinese",
+    A: 98,
+    B: 130,
+    fullMark: 150
+  },
+  {
+    subject: "English",
+    A: 86,
+    B: 130,
+    fullMark: 150
+  },
+  {
+    subject: "Geography",
+    A: 99,
+    B: 100,
+    fullMark: 150
+  },
+  {
+    subject: "Physics",
+    A: 85,
+    B: 90,
+    fullMark: 150
+  },
+  {
+    subject: "History",
+    A: 65,
+    B: 85,
+    fullMark: 150
+  }
 ];
 
 
 const IndexPage = () => {
 
-  console.log(receipts);
+  console.log(data);
 
   return (
     <Layout>
       <div>
-        <h1>
+        {/* <h1>
           Welcome to <b>VeryFi Demo!</b>
-        </h1>
-      </div>
-      <div>
+        </h1> */}
+        <RadarChart
+          cx={300}
+          cy={250}
+          outerRadius={150}
+          width={500}
+          height={500}
+          data={data}
+        >
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar
+            name="Mike"
+            dataKey="A"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+        </RadarChart>
       </div>
 
-      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <CartesianGrid stroke="#ccc" />
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-      </LineChart>
+
 
     </Layout>
   )
