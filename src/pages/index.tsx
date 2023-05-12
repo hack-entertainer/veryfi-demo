4def66e1597da5453f6b0eb91d78d2eae23bb8d5
seawesel <jrodperalta@gmail.com>
2023-05-12 15:53:07 -0400

    `

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,45 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <div>
-      <h1>
-        Welcome to <b>VeryFi Demo!</b>
-      </h1>
-    </div>
-  </Layout>
-)
+import { receipts } from '../response.js';
+
+
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+const data = [
+  { name: 'Page A', uv: 400, pv: 400, amt: 2400 },
+  { name: 'Page B', uv: 500, pv: 400, amt: 2500 },
+  { name: 'Page C', uv: 600, pv: 400, amt: 2400 },
+  { name: 'Page D', uv: 700, pv: 400, amt: 2300 },
+  { name: 'Page E', uv: 800, pv: 400, amt: 2400 },
+  { name: 'Page F', uv: 900, pv: 400, amt: 2500 }
+];
+
+
+const IndexPage = () => {
+
+  console.log(receipts);
+
+  return (
+    <Layout>
+      <div>
+        <h1>
+          Welcome to <b>VeryFi Demo!</b>
+        </h1>
+      </div>
+      <div>
+      </div>
+
+      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <CartesianGrid stroke="#ccc" />
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+
+    </Layout>
+  )
+}
 
 /**
  * Head export to define metadata for the page
