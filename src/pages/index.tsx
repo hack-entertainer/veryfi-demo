132bdd145919ec38299cf7dcf09fb1a7e1520e83
seawesel <jrodperalta@gmail.com>
2023-05-12 15:05:04 -0400

    `

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-// import * as styles from "../components/index.module.css"
 
 const IndexPage = () => (
   <Layout>
-    {/* <div className={styles.textCenter}> */}
     <div>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
       <h1>
         Welcome to <b>VeryFi Demo!</b>
       </h1>
